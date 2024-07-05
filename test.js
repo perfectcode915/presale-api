7affe9a87735184c34591a60c2261282beb4c113
perfectcode915 <kifo1881@outlook.com>
2024-07-05 23:44:07 +0900

    change pending to block

--- test.js
+++ test.js
@@ -10,33 +10,43 @@ const initMain = async () => {
   const currentBalance = await wss_provider.getBalance(MY_WALLET);
   console.log("CURRENT =>", ethers.utils.formatEther(currentBalance));
 
-  wss_provider.on("pending", async (tx) => {
-    wss_provider
-      .getTransaction(tx)
-      .then(async function (transaction) {
-        try {
-          if (transaction && transaction.to === MY_WALLET) {
-            console.log(
-              "value =>",
-              ethers.utils.formatEther(transaction.value)
-            );
-            wss_provider.once(transaction.hash, async (tx) => {
-              console.log("from =>", tx.from);
-              console.log("tx =>", tx.transactionHash);
-              const changedBalance = await wss_provider.getBalance(MY_WALLET);
-              console.log(
-                "INCREASED =>",
-                ethers.utils.formatEther(changedBalance)
-              );
-            });
-          }
-        } catch (err) {
-          console.log("[ERROR]->wssProvidergetTransaction function");
-        }
-      })
-      .catch((error) => {
-        console.log("[ERROR in wssprovider]");
-      });
+  // wss_provider.on("pending", async (tx) => {
+  //   wss_provider
+  //     .getTransaction(tx)
+  //     .then(async function (transaction) {
+  //       try {
+  //         if (transaction && transaction.to === MY_WALLET) {
+  //           console.log(
+  //             "value =>",
+  //             ethers.utils.formatEther(transaction.value)
+  //           );
+  //           wss_provider.once(transaction.hash, async (tx) => {
+  //             console.log("from =>", tx.from);
+  //             console.log("tx =>", tx.transactionHash);
+  //             const changedBalance = await wss_provider.getBalance(MY_WALLET);
+  //             console.log(
+  //               "INCREASED =>",
+  //               ethers.utils.formatEther(changedBalance)
+  //             );
+  //           });
+  //         }
+  //       } catch (err) {
+  //         console.log("[ERROR]->wssProvidergetTransaction function");
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log("[ERROR in wssprovider]");
+  //     });
+  // });
+  wss_provider.on("block", async (block) => {
+    const newBlock = await wss_provider.getBlockWithTransactions(block);
+    for (const tx of newBlock.transactions) {
+      if (tx.to === MY_WALLET) {
+        console.log("VALUE =>", ethers.utils.formatEther(tx.value));
+        console.log("FROM =>", tx.from);
+        console.log("TxHASH =>", tx.hash);
+      }
+    }
   });
 };
 
